fb7069059e5bc0daca49c2fd1a6b3b3de1dd1306
Cole Wyeth <cwye1801@mpsedu.org>
2016-06-08 10:49:47 -0500

    Add files via upload

---
+++ NeuralNet.py
@@ -0,0 +1,24 @@
+#This is a rudimentary neural network. I believe that I could solve more difficult problems by following the same format. 
+
+n1 = [0,0]
+n2 = [0,0]
+neurons= [n1, n2]
+trainingSet = input("How many training vectors would you like to input?")
+
+for i in range(0,int(trainingSet)):
+    trainingVector = list(input("Input a vector without brackets"))
+    trainingSolution = list(input("Input the desired solution (In the same form, orthonormal)."))
+    
+for neuron in range(2):
+    for weight in range(2):
+        neurons[neuron][weight]+= int(trainingSolution[neuron])*int(trainingVector[weight])
+        #Chooses the neuron and the weight          Training solution must depend on neuron, as the neurons are the basis of the output.
+
+  
+print(neurons)            
+            
+            
+    
+
+
+        
